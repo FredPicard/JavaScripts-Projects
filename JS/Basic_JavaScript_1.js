4dcbe5a82542c9019f4192a25e3b4bfd0cee99eb
Fred Picard <50530374+FredPicard@users.noreply.github.com>
2019-08-09 16:36:31 -0700

    Add files via upload

---
+++ JS/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+ var pets = "My Dogs", Dog1="Lynus", Dog2="Henri", Dog3="AJ";
+ var Dog3 = Dog3.fontcolor("green");
+ var Dog2 = Dog2.fontcolor("blue");
+  var Dog1 = Dog1.fontcolor("red");
+ document.write(Dog2)
+ document.write(Dog1)
+ document.write(Dog3)
+ document.write(3+4)
