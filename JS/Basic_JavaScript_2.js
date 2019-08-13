c0ae2f1cbc6cebc8508af00d2bf1cd7f71f59c85
Fred Picard <50530374+FredPicard@users.noreply.github.com>
2019-08-13 15:09:52 -0700

    updated files
    redid the Math to 3

---
+++ JS/Basic_JavaScript_2.js
@@ -0,0 +1,7 @@
+myFunction();
+
+function myFunction() {
+  var dogName = "AppleJack";
+  document.getElementById("demo1").innerHTML =
+  typeof dogName + "My dog " + dogName;
+}
---
+++ JS/Basic_JavaScript_3.js
@@ -0,0 +1,43 @@
+function addition_Function() {
+    var addition = 6 + 4;
+    document.getElementById("Math").innerHTML = addition;
+}
+function subtraction_Function() {
+    var Subtraction = 20 - 7;
+    document.getElementById("Math").innerHTML = Subtraction;
+}
+function multiplication() {
+    var x = 9 * 5;
+    document.getElementById("Math").innerHTML = x;
+}   
+function division() {
+    var x = 45 / 5;
+    document.getElementById("Math").innerHTML = x;
+}   
+function more_Math() {
+    var all_Math = (5 + 30) * 4 / 2 -10;
+document.getElementById("Math").innerHTML = + all_Math;
+}
+function modulus_Operator() {
+    var x = 18 / 5;
+    document.getElementById("Math").innerHTML = + x;   
+}
+function negation_Operator() {
+    var y = 99;
+    document.getElementById("Math").innerHTML = - y;     
+}
+var z = 9;
+z++;
+document.write(z);
+
+var m = 8.45;
+m--;
+document.write(m);
+
+window.alert(Math.random());
+
+window.alert(Math.random() * 571);
+
+function myFunction() {
+    document.getElementById("Math").innerHTML = Math.abs(-25);
+  }
