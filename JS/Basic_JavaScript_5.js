b3cb7b4560b25a7ff68bc50a8c9509c353963f2b
Fred Picard <50530374+FredPicard@users.noreply.github.com>
2019-08-14 10:01:56 -0700

    Added files 5
    Added HTML 5 and JS5 files

---
+++ JS/Basic_JavaScript_5.js
@@ -0,0 +1,48 @@
+ document.write(typeof "Winner,Winner, Chicken Dinner!");
+
+document.write(4E321);
+
+document.write(-3E667)
+
+document.write(7 > 3);
+
+document.write(7 < 3);
+
+console.log(9 * 8);
+
+document.write("30" + 30);
+
+console.log(29 < 5);
+
+document.write(12 ==12);
+
+document.write(12 == 11);
+
+x = 5;
+y = 5;
+document.write(x ===y);
+
+x = "hello";
+y = 5;
+document.write(x === y)
+
+x = "5";
+y = 5;
+document.write(x ===y);
+
+x = 5;
+y = 6;
+document.write(x === y);
+
+document.write(10 > 2 && 11 > 3);
+document.write(10 < 2 && 11 > 3);
+
+document.write(10 > 2 || 11 > 3);
+document.write(10 < 2 || 11 < 3);
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(10 > 11);
+}
+  function not2_Function() {
+     document.getElementById("Not2").innerHTML = !(10 < 11); 
+}
